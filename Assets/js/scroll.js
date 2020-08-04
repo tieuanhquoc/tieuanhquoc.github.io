d846f8a43fbf27d269a27d58fbb8a797a7ea7999
Quoc <Quoc@DESKTOP-DLB5RM3>
2020-08-04 15:08:55 +0700

    a

--- Assets/js/scroll.js
+++ Assets/js/scroll.js
@@ -1,1 +1,37 @@
-!function(e){var t=e(".section"),n=0,o=!1,i=function(){setTimeout(function(){o=!1},300)};document.addEventListener("wheel",function(r){var f=e(t[n]);if(o)r.preventDefault();else if(-r.deltaY<0){if(n+1>=t.length)return;if(!(f[0].getBoundingClientRect().bottom<=e(window).height()))return;r.preventDefault();var u=e(t[++n]).offset().top;o=!0,e("html, body").animate({scrollTop:u},1e3,i)}else{if(n-1<0)return;if(!function(e){return e[0].getBoundingClientRect().top>=0}(f))return;r.preventDefault();u=e(t[--n]).offset().top;o=!0,e("html, body").animate({scrollTop:u},1e3,i)}},{passive:!1})}(jQuery);
+!(function (e) {
+  var t = e(".section"),
+    n = 0,
+    o = !1,
+    i = function () {
+      setTimeout(function () {
+        o = !1;
+      }, 300);
+    };
+  document.addEventListener(
+    "wheel",
+    function (r) {
+      var f = e(t[n]);
+      if (o) r.preventDefault();
+      else if (-r.deltaY < 0) {
+        if (n + 1 >= t.length) return;
+        if (!(f[0].getBoundingClientRect().bottom <= e(window).height()))
+          return;
+        r.preventDefault();
+        var u = e(t[++n]).offset().top;
+        (o = !0), e("html, body").animate({ scrollTop: u }, 200, i);
+      } else {
+        if (n - 1 < 0) return;
+        if (
+          !(function (e) {
+            return e[0].getBoundingClientRect().top >= 0;
+          })(f)
+        )
+          return;
+        r.preventDefault();
+        u = e(t[--n]).offset().top;
+        (o = !0), e("html, body").animate({ scrollTop: u }, 600, i);
+      }
+    },
+    { passive: !1 }
+  );
+})(jQuery);
