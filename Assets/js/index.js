64d374f7b2feeacc20f6e7a46a594871b991fba6
Quoc <Quoc@DESKTOP-DLB5RM3>
2020-08-04 13:56:14 +0700

    a

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -2,9 +2,3 @@ var scrollNow = 0;
 $(document).ready(function () {
     $("html, body").animate({ scrollTop: 0 }, "slow");
 });
-
-var scroll = $('html').scrollTop();
-$(".navbar li a").click(function () {
-    $(".navbar ul li a").removeClass('selected');
-    $(this).addClass('selected');
-})
